ee2545139bd9fb0843057463acb95fd5b9c30809
otumian <popecan1000@gmail.com>
2021-10-16 07:41:21 +0000

    Integrate the joke api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-router.use("/thecatsapi", require("./thecatapi"));
-router.use("/thejokesapi", require("./thejokeapi"));
-router.use("/themealdbapi", require("./themealdbapi"));
-router.use("/thenewsapi", require("./thenewsapi"));
-router.use("/thenumbersapi", require("./thenumbersapi"));
+router.use("/thecatsapi", require("./thecatapi").router);
+router.use("/thejokesapi", require("./thejokeapi").router);
+router.use("/themealdbapi", require("./themealdbapi").router);
+router.use("/thenewsapi", require("./thenewsapi").router);
+router.use("/thenumbersapi", require("./thenumbersapi").router);
 
 module.exports = { router };
